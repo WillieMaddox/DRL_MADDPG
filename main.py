9ddbdc11f62272115eda3181099588f3854e3d4b
WillieMaddox <willie.maddox@gmail.com>
2019-01-04 03:20:48 -0600

    fix: bypass workspace wrappers so we can work locally

--- main.py
+++ main.py
@@ -10,9 +10,6 @@ from tensorboardX import SummaryWriter
 import os
 from utilities import transpose_list, transpose_to_tensor
 
-# keep training awake
-from workspace_utils import keep_awake
-
 # for saving gif
 import imageio
 
@@ -82,7 +79,7 @@ def main():
     timer = pb.ProgressBar(widgets=widget, maxval=number_of_episodes).start()
 
     # use keep_awake to keep workspace from disconnecting
-    for episode in keep_awake(range(0, number_of_episodes, parallel_envs)):
+    for episode in range(0, number_of_episodes + parallel_envs, parallel_envs):
 
         timer.update(episode)
 
@@ -93,7 +90,7 @@ def main():
         # for calculating rewards for this particular episode - addition of all time steps
 
         # save info or not
-        save_info = (episode % save_interval < parallel_envs or episode == number_of_episodes - parallel_envs)
+        save_info = (episode % save_interval < parallel_envs)
         frames = []
         tmax = 0
         
